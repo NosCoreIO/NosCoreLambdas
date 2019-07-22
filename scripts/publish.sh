c20e0e3d31521f8af478568560d0f358c2e36dfa
0Lucifer0 <erwan.joly@gmail.com>
2019-07-22 22:35:19 +1200

    try to fix pip

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-sudo apt install python3-pip
-pip3 install --user awscli
+sudo apt install python-pip
+sudo pip install --user awscli
+eval $(aws ecr get-login --region us-west-2 --no-include-email)
 
 cd NosCore.Travis
 dotnet lambda package
