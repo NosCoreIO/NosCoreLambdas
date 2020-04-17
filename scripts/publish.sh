98ec969bbfc8398f8ca90101b12c309c9a118890
Erwan JOLY <35202750+erwan-joly@users.noreply.github.com>
2020-04-17 21:12:08 +1200

    Update publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
 
-export DOTNET_ROOT=/snap/dotnet-sdk/current
-export MSBuildSDKsPath=$DOTNET_ROOT/sdk/$(${DOTNET_ROOT}/dotnet --version)/Sdks
-export PATH="${PATH}:${DOTNET_ROOT}"
-export PATH="$PATH:$HOME/.dotnet/tools"
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
-brew --version
-brew tap aws/tap
-brew install aws-sam-cli
-sam --version
-
 mkdir -p ~/.aws
 cat > ~/.aws/credentials << EOL
 [default]
