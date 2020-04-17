e11e3f8d821ea7a293acd0ec70f3229b73a7e664
Erwan JOLY <35202750+erwan-joly@users.noreply.github.com>
2020-04-17 21:36:34 +1200

    Update publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)	
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)	
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile	
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile	
+brew --version	
+brew tap aws/tap	
+brew install aws-sam-cli	
+sam --version
+
 mkdir -p ~/.aws
 cat > ~/.aws/credentials << EOL
 [default]
