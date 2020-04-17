7f0201fb3faaaf0d303afaab27c6499f7b195512
Erwan JOLY <35202750+erwan-joly@users.noreply.github.com>
2020-04-17 20:29:41 +1200

    Update publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+export DOTNET_ROOT=/snap/dotnet-sdk/current
+export MSBuildSDKsPath=$DOTNET_ROOT/sdk/$(${DOTNET_ROOT}/dotnet --version)/Sdks
+export PATH="${PATH}:${DOTNET_ROOT}"
+export PATH="$PATH:$HOME/.dotnet/tools"
+
 mkdir -p ~/.aws
 cat > ~/.aws/credentials << EOL
 [default]
